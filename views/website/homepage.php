01dff1a75986ad982c1ee7d7746967b570b810a9
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-02-10 20:31:08 -0300

    Add our code to render RD Station form

--- views/website/homepage.php
+++ views/website/homepage.php
@@ -5,3 +5,18 @@
  * every php file to build header and footer. Our backend already do this.
  */
 ?>
+
+<!-- 
+    This code renders our RD Station acquisition form. 
+    All style should be changed according to the HTML and CSS generated here. 
+-->
+<div role="main" id="newtimes-formulario-leads-page-9cc80504cb28de1971aa"></div>
+
+<!-- 
+    Based on the fact that the following divs are script, we should leave them
+    loading at bottom of this page. 
+-->
+<script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script>
+<script type="text/javascript">
+    new RDStationForms('newtimes-formulario-leads-page-9cc80504cb28de1971aa', 'UA-154439101-1').createForm();
+</script>
