2cf09fb20ef514a27cbd42af0e3447c926f1f18f
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-04 16:20:47 -0300

    ⚙️ Include changes on targets and callout

--- views/website/partials_mentoria/_first_callout.php
+++ views/website/partials_mentoria/_first_callout.php
@@ -1,4 +1,14 @@
 <section id="first_callout">
-<h1>Metodo ADT - <small>Avaliação, Diagnóstico diferenciado e tratamento<small></h1>
-<h2>Domine a terapia manual sendo mais efetivo nos seus tratamentos e aumente em até 3x seu faturamento</h2>
+
+<h1>
+    <span>Método ADT</span>
+    <small>Avaliação, Diagnóstico diferenciado e Tratamento</small>
+</h1>
+
+<h2>
+    Domine a terapia manual sendo mais efetivo nos seus tratamentos e <span>aumente em até 3x seu faturamento</span>
+</h2>
+
+<button class="button-sales-callout">Quero me inscrever no treinamento</button>
+
 </section>
--- views/website/partials_mentoria/_targets.php
+++ views/website/partials_mentoria/_targets.php
@@ -1,5 +1,10 @@
 <!-- @@ Targets :: Start -->
 <section class="content-section" id="targets-container">
     <h1>Para quem é esse treinamento?</h1>
+
+    <div id="target-item">
+        <p>Para todos os fisioterapeutas que queiram se destacar pela sua efetividade 
+            clínica e rapidez na melhora do paciente.</p>
+    </div>
 </section>
 <!-- @@ Targets :: End -->
