3c854878fffe83903a8239cd3d5dc1756cf9ff78
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-04 11:38:11 -0300

    ⚙️ Add new stylesheet and fix header order at link tag

--- views/layout/header.php
+++ views/layout/header.php
@@ -10,12 +10,11 @@
 
     <title>%APP_TITLE%</title>
 
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;700&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap">
     <link rel="stylesheet" href="assets/reset.css">
     <link rel="stylesheet" href="assets/app.css">
-
+    <link rel="stylesheet" href="assets/mentoria.css">
 </head>
 
 <body>
