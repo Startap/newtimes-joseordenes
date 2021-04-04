cf15155aebfd3d28e29c6cdcd66bbfb4007a0a38
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-04 11:33:01 -0300

    🚧 Start new website with routes and controller

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php
 
 use Components\Router;
-use Components\Application;
 
 /** Call methods inside PageController when hits some path mapped on argument 1 */
 Router::route('/', '\Components\Controllers\PageController@getHomepage');
 Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
+Router::route('/mentoria', '\Components\Controllers\PageController@getMentoria');
---
+++ views/website/mentoria.php
@@ -0,0 +1,16 @@
+
+<!-- WEBSITE SECTIONS -->
+
+<?php
+
+use Components\Application;
+
+include(Application::getAppPath('views/website/partials_mentoria/_first_callout'));
+include(Application::getAppPath('views/website/partials_mentoria/_testimonials'));
+include(Application::getAppPath('views/website/partials_mentoria/_targets'));
+include(Application::getAppPath('views/website/partials_mentoria/_course_modules'));
+include(Application::getAppPath('views/website/partials_mentoria/_bonus_content'));
+include(Application::getAppPath('views/website/partials_mentoria/_investiment_budget'));
+include(Application::getAppPath('views/website/partials_mentoria/_instructor_panel'));
+include(Application::getAppPath('views/website/partials_mentoria/_faq_content'));
+include(Application::getAppPath('views/website/partials_mentoria/_disclaimer'));
