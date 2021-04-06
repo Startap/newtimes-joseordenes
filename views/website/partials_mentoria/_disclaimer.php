c6a6a6140d5b1eb77221525af60e974f7d947310
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-06 17:14:42 -0300

    🔠 Change section title

--- views/website/partials_mentoria/_disclaimer.php
+++ views/website/partials_mentoria/_disclaimer.php
@@ -1,5 +1,5 @@
 <!-- @@ Disclaimer :: Start -->
 <section class="content-section" id="disclaimer-container">
-    <h1>Informações sobre obtenção de resultados</h1>
+    <h1>Disclaimers importantes</h1>
 </section>
 <!-- @@ Disclaimer :: End -->
