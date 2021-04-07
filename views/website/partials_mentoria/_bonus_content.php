4f7c3bdd51b6ac8b9ba006da4a5e5be4a326207d
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-07 11:47:03 -0300

    🚧 Fix mobile layout

--- views/website/partials_mentoria/_bonus_content.php
+++ views/website/partials_mentoria/_bonus_content.php
@@ -3,11 +3,11 @@
     <h1>E tem bônus exclusivo para alunos</h1>
 
     <ul id="bonus-items">
-        <li data-brightness="1"><span>Precificação dos serviços de reabilitação</span></li>
-        <li data-brightness="2"><span>3 formas práticas de ganhar dinheiro e experiência na Fisioterapia</span></li>
-        <li data-brightness="3"><span>Marketing de Fisioterapia na prática - como usar as redes sociais para atrair clientes</span></li>
-        <li data-brightness="4"><span>Cuidados e Vedações do Código de Ética da Fisioterapia</span></li>
-        <li data-brightness="5"><span>Organização para seu próprio consultório de reabilitação</span></li>
+        <li><span>Precificação dos serviços de reabilitação</span></li>
+        <li><span>3 formas práticas de ganhar dinheiro e experiência na Fisioterapia</span></li>
+        <li><span>Marketing de Fisioterapia na prática - como usar as redes sociais para atrair clientes</span></li>
+        <li><span>Cuidados e Vedações do Código de Ética da Fisioterapia</span></li>
+        <li><span>Organização para seu próprio consultório de reabilitação</span></li>
     </ul>
 </section>
 <!-- @@ Bonus content :: End -->
