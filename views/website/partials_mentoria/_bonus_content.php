86cb6ffb85c9b99c159953fc73b24456b9f3e6b0
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-06 21:51:42 -0300

    🧩 New structure to bonus modules and some unfinished work

--- views/website/partials_mentoria/_bonus_content.php
+++ views/website/partials_mentoria/_bonus_content.php
@@ -1,5 +1,13 @@
 <!-- @@ Bonus content :: Start -->
 <section class="content-section" id="bonus-container">
     <h1>E tem bônus exclusivo para alunos</h1>
+
+    <ul id="bonus-items">
+        <li data-brightness="1"><span>Precificação dos serviços de reabilitação</span></li>
+        <li data-brightness="2"><span>3 formas práticas de ganhar dinheiro e experiência na Fisioterapia</span></li>
+        <li data-brightness="3"><span>Marketing de Fisioterapia na prática - como usar as redes sociais para atrair clientes</span></li>
+        <li data-brightness="4"><span>Cuidados e Vedações do Código de Ética da Fisioterapia</span></li>
+        <li data-brightness="5"><span>Organização para seu próprio consultório de reabilitação</span></li>
+    </ul>
 </section>
 <!-- @@ Bonus content :: End -->
--- views/website/partials_mentoria/_disclaimer.php
+++ views/website/partials_mentoria/_disclaimer.php
@@ -1,5 +1,5 @@
 <!-- @@ Disclaimer :: Start -->
 <section class="content-section" id="disclaimer-container">
-    <h1>Disclaimers importantes</h1>
+    <h1>Informações importantes</h1>
 </section>
 <!-- @@ Disclaimer :: End -->
