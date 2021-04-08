14ceace67b204e93a1ceb1e03afcf4c830a01915
André Paul Grandsire <andrepg@users.noreply.github.com>
2021-04-08 17:59:28 -0300

    ⚙️ Include new route

--- routes.php
+++ routes.php
@@ -5,4 +5,5 @@ use Components\Router;
 /** Call methods inside PageController when hits some path mapped on argument 1 */
 Router::route('/', '\Components\Controllers\PageController@getHomepage');
 Router::route('/obrigado', '\Components\Controllers\PageController@getAfterSubscription');
-Router::route('/mentoria', '\Components\Controllers\PageController@getMentoria');
+Router::route('/metodoadt', '\Components\Controllers\PageController@getMentoria');
+Router::route('/metodo-adt', '\Components\Controllers\PageController@getMentoria');
